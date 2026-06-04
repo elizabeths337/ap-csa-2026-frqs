e7a6d17aa168b7d295738ed015ae51982146be23
elizabeths337 <elizabeths337@nycstudents.net>
2026-06-04 15:20:33 -0400

    Update courserecord.java

--- question-3/courserecord.java
+++ question-3/courserecord.java
@@ -1,24 +1,14 @@
 public int moreHistoryThanMathAbsences() {
-  int hisCount = 0;
-  int mathCount = 0;
+  int count = 0;
 
   for (int i = 0; i < historyList.size(); i++) {
-    if (historyList.get(i).getStudentID() in mathList) {
-      hisCount += historyList.get(i).getAbsences();
+      for (int j = 0; j < mathList.size(); j++) {
+        if (historyList.get(i).getStudentId() == mathList.get(j).getStudentId()) {
+          if (historyList.get(i).getAbsences() > mathList.get(j).getAbsences()) {
+            count++;
+          }
+        }
+      }
     }
-    
-  for (int i = 0; i < mathList.size(); i++) {
-    if (mathList.get(i).getStudentID() in historyList) {
-      mathList += historyList.get(i).getAbsences();
-    }
-    
-    if (hisCount > mathCount) {
-      return hisCount - mathCount;
-    } else if (mathCount > hisCount) {
-      return mathCount - hisCount;
-    } else {
-      return 0;
-    }
-    
-  }  
+    return count; 
 }
