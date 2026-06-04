93dd43b97442f9c6b0f8e92acf122c4825585c89
elizabeths337 <elizabeths337@nycstudents.net>
2026-06-04 15:28:48 -0400

    Update gameboard.java

--- question-4/gameboard.java
+++ question-4/gameboard.java
@@ -1,14 +1,14 @@
 public int getPointsForRow(int targetRow) {
   int count = 0;
   Boolean isCol = True;
-  for (int i = 0; i < board.size(); i++) {
-    String sameCol = board.[targetRow][0].get(Color);
+  for (int i = 0; i < board.length; i++) {
+    String sameCol = board[targetRow][0].getColor();
     count += board[targetRow][i].getPoints();
-    if (! sameCol.equals(board[targetRow][i].get(Color))
+    if (! sameCol.equals(board[targetRow][i].getColor();
       isCol = False;
   }
   if (isCol) {
-    return 2 * count
+    return 2 * count;
   }
   else {
     return count;
