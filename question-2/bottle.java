d4a8960cfea28b14034929c775f0db0204647fd9
elizabeths337 <elizabeths337@nycstudents.net>
2026-06-04 12:05:37 -0400

    Update bottle.java

--- question-2/bottle.java
+++ question-2/bottle.java
@@ -3,12 +3,13 @@ public class Bottle {
   private double curr;
 
   public Bottle (double amount) {
-    mL = amount;    
+    mL = amount;
+    curr = amount;
   }
   
   public double updateAmount(double less) {
     curr -= less;
-    if (curr >= (mL * 0.25) {
+    if (curr >= (mL * 0.25)) {
         curr = mL;
     }
     return curr;
